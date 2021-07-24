86cbcebac9faef121278710312e8561b4443fe9a
kodhanjo <felix.osambo@student.moringaschool.com>
2021-07-24 19:16:12 +0300

    profile components updated

--- src/app/Details/github-profile/github-profile.component.ts
+++ src/app/Details/github-profile/github-profile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from 'src/app/Services/git-search.service';
+import { of, fromEvent, Observable} from 'rxjs';
+import { merge } from 'rxjs';
+import { mapTo} from 'rxjs/operators';
+import { User} from '../../Models/user.model';
+import { Repository } from '../../Models/repository';
 
 @Component({
   selector: 'app-github-profile',
@@ -7,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor() { }
+  online$: Observable<boolean>;
+  status!: string;
+  isConnected = true;
+  details!: User;
+  repos!: Repository[];
+  loading = false;
+
+  constructor(private gitSearchService: GitSearchService) { 
+    this.online$ = merge(
+      of(navigator.onLine),
+      fromEvent(window, 'online').pipe(mapTo(true)),
+      fromEvent(window, 'offline').pipe(mapTo(false))
+      );
+    this.networkStatus();
+  }
+  public networkStatus(): void {
+    this.online$.subscribe(value => {
+      if (value) {
+        this.isConnected = true;
+        this.status = 'ONLINE';
+      } else {
+        this.isConnected = false;
+        this.status = 'OFFLINE';
+      }
+    });
+  }
 
   ngOnInit(): void {
+    this.loading =  true;
+    this.gitSearchService.getMyInfo().then(data => {
+      this.details = data;
+    });
+    this.gitSearchService.getMyRepos().then(repos => {
+      this.repos = repos;
+      this.loading = false;
+    });
   }
 
 }
