d7bc82f541c7d8f99ac239c8559c915a2c8b6174
kodhanjo <felix.osambo@student.moringaschool.com>
2021-07-24 17:31:24 +0300

    updated directive and removed strict on the string

--- src/app/Directives/loading-color-change.directive.ts
+++ src/app/Directives/loading-color-change.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appLoadingColorChange]'
@@ -6,5 +6,14 @@ import { Directive } from '@angular/core';
 export class LoadingColorChangeDirective {
 
   constructor() { }
+  colors = [
+    'red', 'orange', 'yellow', 'green', 'blue',
+    'purple', 'violet'
+  ];
 
+  @HostBinding('style.color') color!: string;
+  @HostBinding('style.border-color') borderColor!: string;
+
+  @HostListener('hover') changeColor  = () =>
+    (this.color = this.borderColor = this.colors[Math.floor(Math.random() * this.color.length)])  
 }
