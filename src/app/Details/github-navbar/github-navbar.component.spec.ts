eb34f069507f7294b2a5293c84c698046b0fc7a8
kodhanjo <felix.osambo@student.moringaschool.com>
2021-07-24 15:53:24 +0300

    generated github-nav bar components

---
+++ src/app/Details/github-navbar/github-navbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GithubNavbarComponent } from './github-navbar.component';
+
+describe('GithubNavbarComponent', () => {
+  let component: GithubNavbarComponent;
+  let fixture: ComponentFixture<GithubNavbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GithubNavbarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GithubNavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
 import { GithubProfileComponent } from './Details/github-profile/github-profile.component';
 import { GithubSearchComponent } from './Details/github-search/github-search.component';
+import { GithubNavbarComponent } from './Details/github-navbar/github-navbar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { GithubSearchComponent } from './Details/github-search/github-search.com
     HomeComponent,
     DetailsComponent,
     GithubProfileComponent,
-    GithubSearchComponent
+    GithubSearchComponent,
+    GithubNavbarComponent
   ],
   imports: [
     BrowserModule,
