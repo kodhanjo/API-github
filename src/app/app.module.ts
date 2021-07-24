1d98457885d850a74e08ec6565023485df5290ce
kodhanjo <felix.osambo@student.moringaschool.com>
2021-07-24 15:52:33 +0300

    generated github search engine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
 import { GithubProfileComponent } from './Details/github-profile/github-profile.component';
+import { GithubSearchComponent } from './Details/github-search/github-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     DetailsComponent,
-    GithubProfileComponent
+    GithubProfileComponent,
+    GithubSearchComponent
   ],
   imports: [
     BrowserModule,
