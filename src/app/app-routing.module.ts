f7f2738fb7debc3db14c6d784f70439897628b3a
kodhanjo <felix.osambo@student.moringaschool.com>
2021-07-24 18:44:59 +0300

    updated app-route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubProfileComponent } from './Details/github-profile/github-profile.component';
+import { GithubSearchComponent } from './Details/github-search/github-search.component';
+import { PageNotFoundComponent } from './Details/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component: GithubProfileComponent},
+  { path:'search', component: GithubSearchComponent},
+  { path:'', redirectTo:'home', pathMatch:'full'},
+  { path:'**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
